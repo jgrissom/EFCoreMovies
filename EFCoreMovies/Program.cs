e5a15ba71ef089c247b58f32a13ea407ae49207d
jgrissom <jgrissom91@gmail.com>
2019-04-03 19:50:32 -0500

    NLog & movies.csv
    1) Log program start & end
    2) Import movies.csv (set property - copy if newer)

--- EFCoreMovies/Program.cs
+++ EFCoreMovies/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using NLog;
 
 namespace EFCoreMovies
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            logger.Info("Program started");
+
             Console.WriteLine("Hello World!");
+
+            logger.Info("Program ended");
         }
     }
 }
