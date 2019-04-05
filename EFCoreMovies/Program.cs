dc30489711bb81204070fc5008c0cdd20094328c
jgrissom <jgrissom91@gmail.com>
2019-04-04 23:04:24 -0500

    Create Data Import Program - part 2
    1) Add DBContext
    2) Create static method to insert movie data into database tables
    3) Insert a movie into Movies table for each line in the data file
    4) Insert only unique genres into Genres table
    5) Insert MovieGenre for each genre connected to each movie

--- EFCoreMovies/Program.cs
+++ EFCoreMovies/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using EFCoreMovies.Models;
 using NLog;
 
@@ -12,51 +13,86 @@ namespace EFCoreMovies
         {
             logger.Info("Program started");
 
-            StreamReader sr = new StreamReader("movies.csv");
-            // if the text file is empty, do not seed
-            if (!sr.EndOfStream)
+            using (var context = new MovieContext())
             {
-                // skip first line of data file
-                sr.ReadLine();
-                // read from the data file
-                while (!sr.EndOfStream)
+                StreamReader sr = new StreamReader("movies.csv");
+                // if the text file is empty, do not seed
+                if (!sr.EndOfStream)
                 {
-                    // create instance of Movie class
-                    Movie movie = new Movie();
-                    string line = sr.ReadLine();
-                    // first look for quote(") in string
-                    // this indicates a comma(,) in movie title
-                    int idx = line.IndexOf('"');
-                    if (idx == -1)
+                    // skip first line of data file
+                    sr.ReadLine();
+                    // read from the data file
+                    while (!sr.EndOfStream)
                     {
-                        // no quote = no comma in movie title
-                        // movie details are separated with comma(,)
-                        string[] movieDetails = line.Split(',');
-                        movie.Title = movieDetails[1];
-                        Console.WriteLine($"{movie.Title} - {movieDetails[2].Replace('|', ',')}");
+                        // create instance of Movie class
+                        Movie movie = new Movie();
+                        string line = sr.ReadLine();
+                        // first look for quote(") in string
+                        // this indicates a comma(,) in movie title
+                        int idx = line.IndexOf('"');
+                        if (idx == -1)
+                        {
+                            // no quote = no comma in movie title
+                            // movie details are separated with comma(,)
+                            string[] movieDetails = line.Split(',');
+                            movie.Title = movieDetails[1];
+                            AddMovie(context, movie, movieDetails[2].Split('|'));
+                        }
+                        else
+                        {
+                            // quote = comma or quotes in movie title
+                            // remove movieId and first comma from string
+                            line = line.Substring(idx);
+                            // find the last quote
+                            idx = line.LastIndexOf('"');
+                            // extract title (replace "" with ")
+                            movie.Title = line.Substring(1, idx - 1).Replace("\"\"", "\"");
+                            // remove title and next comma from the string
+                            line = line.Substring(idx + 2);
+                            AddMovie(context, movie, line.Split('|'));
+                        }
+                    }
+                    // close file when done
+                    sr.Close();
+                }
+                else
+                {
+                    logger.Info("Database NOT seeded");
+                }
+                // log results
+                logger.Info($"{context.Movies.Local.Count()} Movies");
+                logger.Info($"{context.Genres.Local.Count()} Genres");
+                logger.Info($"{context.MovieGenres.Local.Count()} MovieGenres");
+            }
+
+            logger.Info("Program ended");
+        }
+
+        private static void AddMovie(MovieContext context, Movie movie, String[] genreText)
+        {
+            // add movie to Movies table
+            context.Movies.Add(movie);
+            foreach (var gt in genreText)
+            {
+                if (gt.ToLower() != "(no genres listed)")
+                {
+                    MovieGenre mg = new MovieGenre() { Movie = movie };
+                    // only add unique genres to Genres table
+                    if (!context.Genres.Local.Any(g => g.GenreText == gt))
+                    {
+                        Genre genre = new Genre() { GenreText = gt };
+                        context.Genres.Add(genre);
+                        mg.Genre = genre;
                     }
                     else
                     {
-                        // quote = comma or quotes in movie title
-                        // remove movieId and first comma from string
-                        line = line.Substring(idx);
-                        // find the last quote
-                        idx = line.LastIndexOf('"');
-                        // extract title (replace "" with ")
-                        movie.Title = line.Substring(1, idx - 1).Replace("\"\"", "\"");
-                        // remove title and next comma from the string
-                        line = line.Substring(idx + 2);
-                        Console.WriteLine($"{movie.Title} - {line.Replace('|', ',')}");
+                        Genre genre = context.Genres.Local.FirstOrDefault(g => g.GenreText == gt);
+                        mg.Genre = genre;
                     }
+                    // add all movie genres to MovieGenres table
+                    context.MovieGenres.Add(mg);
                 }
-                // close file when done
-                sr.Close();
             }
-            else
-            {
-                logger.Info("Database NOT seeded");
-            }
-            logger.Info("Program ended");
         }
     }
 }
